d470bd5d3cb48e7239785861c37947acb4b9f9d1
ruter <arturkummer08@gmail.com>
2025-11-08 22:26:35 +0100

    added bloom

--- app/displays/twodgame.py
+++ app/displays/twodgame.py
@@ -11,15 +11,53 @@ class TwoDGameDisplay(BaseDisplay):
         self.delta_time = rl.get_frame_time()
         self.camera = twodcamera.Camera(self.game.width, self.game.height, self.square_pos[0], self.square_pos[1], 3)
 
+        self.texture =  rl.load_render_texture(game.width, game.height)
+        rl.set_texture_filter(self.texture.texture, rl.TextureFilter.TEXTURE_FILTER_BILINEAR)
+
+        self.bloom_shader = rl.load_shader("", "app/shaders/bloom.fs")
+        self.shader_resolution_location = rl.get_shader_location(self.bloom_shader, "resolution")
+        self.shader_time_location = rl.get_shader_location(self.bloom_shader, "time")
+
+        res = rl.ffi.new("float[2]", [float(self.game.width), float(self.game.height)])
+        rl.set_shader_value(self.bloom_shader, self.shader_resolution_location, res,
+                            rl.ShaderUniformDataType.SHADER_UNIFORM_VEC2)
+
+
+
     def render(self):
+        rl.begin_texture_mode(self.texture)
+
+        rl.clear_background(rl.BLACK)
         self.camera.begin_mode()
+
         rl.draw_fps(10, 10)
         rl.draw_rectangle(int(self.square_pos[0]), int(self.square_pos[1]), 20, 20, rl.RED)
+
         self.camera.end_mode()
+        rl.end_texture_mode()
+
+        #shader stuff
+        rl.begin_shader_mode(self.bloom_shader)
+
+        src = rl.Rectangle(0.0, 0.0,
+                           float(self.texture.texture.width),
+                           -float(self.texture.texture.height))
+        dst = rl.Rectangle(0.0, 0.0, float(self.game.width), float(self.game.height))
+        rl.draw_texture_pro(self.texture.texture, src, dst, rl.Vector2(0.0, 0.0), 0.0, rl.WHITE)
+
+        rl.end_shader_mode()
+
+
 
     def update(self):
         self.delta_time = rl.get_frame_time()
         self.camera.update_target(self.square_pos[0], self.square_pos[1], self.delta_time)
+
+        t = rl.ffi.new("float *", float(rl.get_time()))
+        rl.set_shader_value(self.bloom_shader, self.shader_time_location, t,
+                            rl.ShaderUniformDataType.SHADER_UNIFORM_FLOAT)
+
+
         if rl.is_key_down(rl.KeyboardKey.KEY_W):
             self.square_pos[1] -= self.speed * self.delta_time
         if rl.is_key_down(rl.KeyboardKey.KEY_S):
