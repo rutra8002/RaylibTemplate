88b571c1faf0f245a6d409f75f54364e1215df77
rutra8002 <96191787+rutra8002@users.noreply.github.com>
2025-11-11 18:00:12 +0100

    get out
    Removed commented-out code for Raylib text rendering.

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 from app import game
 
 
-# text = "Hello, Raylib!"
-# font_size = 20
-#
-# while not rl.window_should_close():
-#     text_width = rl.measure_text(text, font_size)
-#     x = (screen_w - text_width) // 2
-#     y = (screen_h - font_size) // 2
-#
-#     rl.begin_drawing()
-#     rl.clear_background(rl.RAYWHITE)
-#     rl.draw_text(text, x, y, font_size, rl.DARKGRAY)
-#     rl.end_drawing()
-#
-# rl.close_window()
 
 if __name__ == '__main__':
     gmae = game.Game()
